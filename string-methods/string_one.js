0380d67853df9befb7e8a77a088c69469566035c
elielaurentraphael <raphelaur@gmail.com>
2022-04-11 00:04:36 +0300

    Exercice n°4 - fichier string_one.js

---
+++ string-methods/string_one.js
@@ -0,0 +1,12 @@
+console.log("somestringtext" [3]); // e
+console.log("éléphant" [1]); // é
+console.log("animal".longueur); // undefined
+console.log("animal".length - 1); // 5
+console.log("yesJOHN".toUpperCase()); //YESJOHN
+console.log("hey friends call me" [2] === "y"); // true
+console.log("animal dans la forêt".length > 20); // false
+console.log("bonjour ranger".indexOf("r"));
+console.log("éléphant".indexOf("e"));
+console.log("chat" [5]); // undefined
+console.log("chat".indexOf("x"));
+console.log("cat".indexOf("C"));
