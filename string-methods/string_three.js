95b495bf5fbb6d6e68307b166de1f473efff6edc
elielaurentraphael <raphelaur@gmail.com>
2022-04-11 08:35:03 +0300

    Rectification affichage console.log

--- string-methods/string_three.js
+++ string-methods/string_three.js
@@ -6,9 +6,9 @@ const i = 7;
 const char = phrase[i];
 console.log(char); // " " (space)
 console.log(phrase.indexOf(char)); // 3
-console.log(phrase.slice(2, 8)); // "t the "
-console.log("abcdefg".slice(1, 3)); // "bc"
-console.log("abcdefg".slice(2)); // "cdefg"
-console.log("abcdefg".slice(4)); // "efg"
-console.log("abcdefg".slice(2, -1)); // "cdef"
-console.log("abcdefg".slice(2, -2)); // "cde"
+console.log(phrase.slice(2, 8)); // t the 
+console.log("abcdefg".slice(1, 3)); // bc
+console.log("abcdefg".slice(2)); // cdefg
+console.log("abcdefg".slice(4)); // efg
+console.log("abcdefg".slice(2, -1)); // cdef
+console.log("abcdefg".slice(2, -2)); // cde
